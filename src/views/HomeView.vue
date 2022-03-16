0e6a3adda73389f2ccad5b558d88466ba012ba68
oliujunk <413041277@qq.com>
2022-03-16 23:17:31 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+import { SerialPort } from 'serialport';
+
+export default {
+  name: 'HomeView',
+
+  mounted() {
+    SerialPort.list().then((ports) => {
+      console.log(ports);
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+}
+
+</style>
